aedc35a9dd283ac52db7923174258bb374dbf950
dhernando <david.hernando91@gmail.com>
2026-03-11 10:30:14 +0100

    remove redundant config assignment

--- cmd/qcloud/main.go
+++ cmd/qcloud/main.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/qdrant/qcloud-cli/internal/cli"
 	"github.com/qdrant/qcloud-cli/internal/state"
-	"github.com/qdrant/qcloud-cli/internal/state/config"
 )
 
 var version = "dev"
@@ -15,7 +14,6 @@ var version = "dev"
 func main() {
 	ctx := context.Background()
 	s := state.New(version)
-	s.Config = config.New()
 	cmd := cli.NewRootCommand(s)
 	cmd.SetContext(ctx)
 	if err := cmd.Execute(); err != nil {
