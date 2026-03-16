8a1a11383d28717ba7e141ea3ee47da3c3248505
dhernando <david.hernando91@gmail.com>
2026-03-16 15:27:06 +0100

    add build workflow to upload snapshot artifacts on PRs and pushes to main without an official release

--- cmd/qcloud/main.go
+++ cmd/qcloud/main.go
@@ -9,11 +9,21 @@ import (
 	"github.com/qdrant/qcloud-cli/internal/state"
 )
 
-var version = "dev" // x-releaser-pleaser-version
+var (
+	version           = "0.3.0" // x-releaser-pleaser-version
+	versionPrerelease = "dev"
+)
+
+func versionString() string {
+	if versionPrerelease != "" {
+		return version + "-" + versionPrerelease
+	}
+	return version
+}
 
 func main() {
 	ctx := context.Background()
-	s := state.New(version)
+	s := state.New(versionString())
 	cmd := cli.NewRootCommand(s)
 	cmd.SetContext(ctx)
 	if err := cmd.Execute(); err != nil {
